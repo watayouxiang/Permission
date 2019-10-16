ddf733e05182c0def1b1dfba77f3b9aff7a61559
watayouxiang <watayouxiang@qq.com>
2019-10-16 15:14:30 +0800

    .

--- app/src/main/java/com/watayouxiang/tpermission/MainActivity.java
+++ app/src/main/java/com/watayouxiang/tpermission/MainActivity.java
@@ -33,13 +33,9 @@ public class MainActivity extends ListActivity {
     private void requestPermissions() {
         mPermissionHelper.requestPermissions(
                 new String[]{
-                        Manifest.permission.ACCESS_NETWORK_STATE,
-                        Manifest.permission.ACCESS_WIFI_STATE,
                         Manifest.permission.READ_PHONE_STATE,
-                        Manifest.permission.INTERNET,
                         Manifest.permission.ACCESS_COARSE_LOCATION,
                         Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.CHANGE_WIFI_STATE,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.CAMERA,
