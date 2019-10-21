350a02478de466524299fe9496085f9f697bbf74
watayouxiang <watayouxiang@qq.com>
2019-10-21 11:14:22 +0800

    .

--- app/src/main/java/com/watayouxiang/tpermission/MainActivity.java
+++ app/src/main/java/com/watayouxiang/tpermission/MainActivity.java
@@ -22,8 +22,6 @@ import java.util.List;
 public class MainActivity extends ListActivity {
     private List<String> mPermissions = Arrays.asList(
             Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
